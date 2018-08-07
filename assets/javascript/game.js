15ca23fdbad0f1179fc0031a5622a068f47a5f39
Counttalkula666 <elenabelgum@gmail.com>
2018-08-06 20:36:56 -0700

    save before changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,34 @@
+
 $('#crystalOne').attr('data-number', lowrandomnumber());
+$('#crystalTwo').attr('data-number', lowrandomnumber());
+$('#crystalThree').attr('data-number', lowrandomnumber());
+$('#crystalFour').attr('data-number', lowrandomnumber());
+
 $('.Crystals').on('click', function() {
     console.log('I work', $(this).attr('data-number'));
+    
 });
 
+//here is my random number generator
 function lowrandomnumber() {
     return Math.floor(Math.random()*12 + 1) 
-
+    console.log(lowrandomnumber())
 }
-console.log(lowrandomnumber())
+
+var targetNumber = function counter(){
+$("#number-to-guess").text(targetNumber);
+}
+
 
 //this is the counter of the user score
-
-var counter = 0;
-$(".Crystals").on("click", function() {
-
-  // Each time the user clicks the crystal the counter goes up by 1.
-  counter += 1;
-
+//$(".Crystals").on("click", function() {
+  //var counter = 0;
+  // Each time the user clicks the crystal the counter needs to add that data !!!!
+  //counter += function lowrandomnumber()
   // We then output the number of times the crystal is clicked.
-  alert("You clicked this crystal " + counter + " times!");
+ 
 
-});
+//});
 
 //trying to create a refresh button below...can i select an image and make that the button? 
 //how does it display otherwise?
